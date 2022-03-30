365c18a17ccde75ec1a4d3ba3d304a47fbdd4942
Oyinbayode <oyinbayode75@gmail.com>
2022-03-30 14:58:41 +0100

    feat: firebase authentication completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import * as ROUTES from "./constants/routes";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 function App() {
+  const { user } = useAuthListener();
+  console.log(user);
+
   return (
     <Router>
       <Routes>
-        <Route exact path={ROUTES.BROWSE} element={<Browse />} />
-        <Route exact path={ROUTES.SIGN_IN} element={<Signin />} />
-        <Route exact path={ROUTES.SIGN_UP} element={<Signup />} />
-        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route
+          path={ROUTES.SIGN_IN}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Signin />
+            </IsUserRedirect>
+          }
+        />
+        <Route
+          path={ROUTES.SIGN_UP}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Signup />
+            </IsUserRedirect>
+          }
+        />
+        <Route
+          path={ROUTES.BROWSE}
+          element={
+            <ProtectedRoute user={user}>
+              <Browse />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          exact
+          path={ROUTES.HOME}
+          element={
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+              <Home />
+            </IsUserRedirect>
+          }
+        />
       </Routes>
     </Router>
   );
