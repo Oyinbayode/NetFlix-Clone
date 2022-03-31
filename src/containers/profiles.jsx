37dbf9646d08b143a757002a142f24b133e01e5e
Oyinbayode <oyinbayode75@gmail.com>
2022-03-31 16:09:05 +0100

    feat: profiling on Browse Page implentation

--- src/containers/profiles.jsx
+++ src/containers/profiles.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import * as ROUTES from "../constants/routes";
-import { Header } from "../components";
+import { Header, Profiles } from "../components";
 import logo from "../logo.svg";
 
 export function SelectProfileContainer({ user, setProfile }) {
   return (
     <>
-      <Header bg={false}></Header>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        </Header.Frame>
+      </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's watching</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User>
+            <Profiles.Picture src={user.photoURL} />
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
     </>
   );
 }
