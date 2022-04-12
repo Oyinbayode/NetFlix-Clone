cb284a39dc4cc196063ee2ce65c6004b29cc2187
Oyinbayode <oyinbayode75@gmail.com>
2022-04-12 15:21:53 +0100

    feat: cards styling

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,31 +1,105 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Title = styled.p`
+  font-size: 24px;
+  color: #e5e5e5;
+  font-weight: bold;
+  margin-left: 56px;
+  margin-right: 56px;
+  margin-top: 0;
+`;
 
-export const Title = styled.p``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+  box-sizing: border-box;
 
-export const Group = styled.div``;
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+
+  > ${Title} {
+    @media screen and (max-width: 1000px) {
+      margin-left: 30px;
+    }
+  }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection }) =>
+    flexDirection === "row" ? "row" : "column"};
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
+  ${({ margin }) => margin && `margin: ${margin}`};
+
+  > ${Container}:first-of-type {
+    @media screen and (min-width: 1100px) {
+      margin-top: -150px;
+    }
+  }
+`;
 
 export const SubTitle = styled.p``;
 
 export const Text = styled.p``;
 
-export const Meta = styled.div``;
+export const Meta = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  background: #00000008f;
+`;
 
 export const Item = styled.div``;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  border: 0;
+  width: 100%;
+  max-width: 305px;
+  cursor: pointer;
+  height: auto;
+  padding: 0;
+  margin: 0;
+`;
 
-export const Entities = styled.div``;
+export const Entities = styled.div`
+  display: flex;
+`;
 
 export const Feature = styled.div``;
 
-export const FeatureTitle = styled(Title)``;
+export const FeatureTitle = styled(Title)`
+  margin-left: 0;
+`;
 
 export const FeatureText = styled.p``;
 
-export const FeatureClose = styled.button``;
+export const FeatureClose = styled.button`
+  color: #fff;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  background: transparent;
+  border: 0 img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+  }
+`;
 
-export const Maturity = styled.div``;
+export const Maturity = styled.div`
+  background: ${({ rating }) => (rating >= 15 ? "red" : "green")};
+  border-radius: 15px;
+  width: 20px;
+  padding: 5px;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  margin-right: 10px;
+  font-size: 12px;
+`;
 
 export const Content = styled.div``;
