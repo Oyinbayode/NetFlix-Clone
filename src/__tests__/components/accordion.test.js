d4eacaf27bb0b613fc36ef06a575ef440adcf7c5
Oyinbayode <oyinbayode75@gmail.com>
2022-04-21 21:50:56 +0100

    card component testing

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -41,7 +41,7 @@ describe("<Accordion />", () => {
     );
 
     const whatIsNetflixBodyText =
-      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert - all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single advert - all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
 
     expect(screen.queryByText(whatIsNetflixBodyText)).toBeFalsy();
     fireEvent.click(screen.queryByText("What is Netflix?"));
