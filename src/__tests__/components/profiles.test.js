f683a49c898e7987abc9cb66eb1234b103cd82ad
Oyinbayode <oyinbayode75@gmail.com>
2022-04-22 16:05:54 +0100

    testing for profiles component completed

--- src/__tests__/components/profiles.test.js
+++ src/__tests__/components/profiles.test.js
@@ -1,3 +1,47 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Profiles } from "../../components";
+import { Profile } from "../../components/header/styles/header";
+
+describe("<Profiles />", () => {
+  it("renders the <Profiles /> with populated data", () => {
+    const { container } = render(
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User onClick={() => {}}>
+            <Profiles.Picture
+              src="/images/karl.png"
+              data-testid="profile-picture"
+            />
+            <Profiles.Name>Karl Hadwen</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    );
+
+    expect(screen.getByText("Who's watching?")).toBeTruthy();
+    expect(screen.getByTestId("profile-picture")).toBeTruthy();
+    expect(screen.getByText("Karl Hadwen")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders the <Profiles /> with populated data but misc profile image", () => {
+    const { container } = render(
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User onClick={() => {}}>
+            <Profiles.Picture data-testid="profile-picture-misc" />
+            <Profiles.Name>Karl Hadwen</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    );
+
+    expect(screen.getByText("Who's watching?")).toBeTruthy();
+    expect(screen.getByTestId("profile-picture-misc")).toBeTruthy();
+    expect(screen.getByText("Karl Hadwen")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
